61ad938f96bb3ca3016dee0fc63c563c68a72276
tashaxing <tashaxing123@163.com>
2017-12-04 20:27:05 +0800

    add

--- build.sbt
+++ build.sbt
@@ -7,5 +7,6 @@ scalaVersion := "2.12.3"
 libraryDependencies ++= Seq(
     "com.typesafe.akka" %% "akka-http" % "10.0.10",
     "com.typesafe.akka" %% "akka-stream" % "2.5.4",
-    "com.typesafe.akka" %% "akka-actor"  % "2.5.4"
+    "com.typesafe.akka" %% "akka-actor"  % "2.5.4",
+    "com.typesafe.akka" %% "akka-http-spray-json" % "10.0.10"
 )
--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,28 +1,68 @@
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model._
-import akka.http.scaladsl.server.Directives._
 import akka.stream.ActorMaterializer
+import akka.Done
+import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import spray.json.DefaultJsonProtocol._
+
 import scala.io.StdIn
+
+import scala.concurrent.Future
 
 object MockServer
 {
-    final case class Item(name: String, id: Int)
-    implicit val itemFormat == jsonFormat1
+    // model
+    final case class User(name: String, age: Int, addr: String)
+    implicit val itemFormat = jsonFormat3(User) // jsonFormatX refers to X number arguments
+
+    final case class UserGroup(items: List[User])
+    implicit val orderFormat = jsonFormat1(UserGroup)
+
+    // http api
+//    def fetchUserGroup(): Future[Option[List[User]]] =
+//    {
+//
+//    }
+//
+//    def fetchUser(user_id: Int): Future[Option[User]] =
+//    {
+//
+//    }
+//
+//    def saveUser(user: User): Future[Done] =
+//    {
+//
+//    }
 
     def main(args: Array[String]): Unit =
     {
         implicit val system = ActorSystem("mock_system")
         implicit val materializer = ActorMaterializer()
+
         // needed for the future flatMap/onComplete in the end
         implicit val executionContext = system.dispatcher
 
-        val route =
-            path("test") {
-                get {
-                    complete("just a test for scala http")
+        val route: Route =
+            get
+            {
+                pathPrefix("")
+                {
+                    complete("the index response")
+                }
+                pathPrefix("list_all")
+                {
+                    val user_group = List(User("jack", 18, "NewYork"), User("mike", 21, "paris"))
+                    complete(user_group)
+                }
+                pathPrefix("user" / LongNumber)
+                {
+                    id => complete(User("lucy", id.toInt, "tokyo"))
                 }
             }
+
 
         val bindingFuture = Http().bindAndHandle(route, "localhost", 7070)
 
