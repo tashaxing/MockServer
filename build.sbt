c298dac60228c1e2cb2fc356b392b853e04114f6
tashaxing <tashaxing123@163.com>
2017-12-05 10:39:29 +0800

    update

--- build.sbt
+++ build.sbt
@@ -8,5 +8,6 @@ libraryDependencies ++= Seq(
     "com.typesafe.akka" %% "akka-http" % "10.0.10",
     "com.typesafe.akka" %% "akka-stream" % "2.5.4",
     "com.typesafe.akka" %% "akka-actor"  % "2.5.4",
-    "com.typesafe.akka" %% "akka-http-spray-json" % "10.0.10"
+    "com.typesafe.akka" %% "akka-http-spray-json" % "10.0.10",
+    "ch.megard" %% "akka-http-cors" % "0.2.2"
 )
--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -6,14 +6,21 @@ import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import akka.http.scaladsl.model.headers.HttpOriginRange
+import ch.megard.akka.http.cors.scaladsl.CorsDirectives.cors
+import ch.megard.akka.http.cors.scaladsl.settings.CorsSettings
 import spray.json.DefaultJsonProtocol._
 
 import scala.io.StdIn
-
-import scala.concurrent.Future
+import scala.collection.mutable
 
 object MockServer
 {
+    // cors setting for other origin access
+    val settings = CorsSettings.defaultSettings.copy(
+        allowedOrigins = HttpOriginRange.* // * refers to all
+    )
+
     // model
     final case class User(name: String, age: Int, addr: String)
     implicit val itemFormat = jsonFormat3(User) // jsonFormatX refers to X number arguments
@@ -37,6 +44,8 @@ object MockServer
 //
 //    }
 
+    private val userGroup = mutable.ListBuffer[User]()
+
     def main(args: Array[String]): Unit =
     {
         implicit val system = ActorSystem("mock_system")
@@ -45,30 +54,65 @@ object MockServer
         // needed for the future flatMap/onComplete in the end
         implicit val executionContext = system.dispatcher
 
+        // define routes, notice there are many ways
         val route: Route =
-            get
+            (path("hello") & get & cors(settings))
             {
-                pathPrefix("")
+                complete("hello akka")
+
+            } ~
+            (path("list_all") & cors(settings))
+            {
+                // simple get
+                get
                 {
-                    complete("the index response")
-                }
-                pathPrefix("list_all")
-                {
-                    val user_group = List(User("jack", 18, "NewYork"), User("mike", 21, "paris"))
+                    // add elem to userGroup, can use this.userGroup += User("jack", 18, "NewYork")
+                    userGroup.clear()
+                    userGroup += User("jack", 18, "NewYork")
+                    userGroup += User("mike", 21, "paris")
+                    val user_group = UserGroup(this.userGroup.toList)
                     complete(user_group)
                 }
-                pathPrefix("user" / LongNumber)
+            } ~
+            get
+            {
+                // get by params using akka http path matcher
+                (pathPrefix("user" / IntNumber ) & cors(settings))
                 {
-                    id => complete(User("lucy", id.toInt, "tokyo"))
+                    age =>
+                    {
+                        val user = User("lucy", age.toInt, "tokyo")
+                        complete(user)
+                    }
+                }
+            } ~
+            post {
+                (path("create_user") & cors(settings))
+                {
+                    entity(as[User])
+                    {
+                        user =>
+                        {
+                            println(user)
+                            // do sth to process
+                            userGroup += user
+                            println(userGroup)
+
+                            complete("done")
+                        }
+                    }
                 }
             }
 
 
+        // bind to ip and port and start server
         val bindingFuture = Http().bindAndHandle(route, "localhost", 7070)
 
         println(s"Server online at http://localhost:7070/\nPress RETURN to stop...")
         StdIn.readLine() // let it run until user presses return
+//        while (1) // or use a dead loop
 
+        // stop server
         bindingFuture
             .flatMap(_.unbind()) // trigger unbinding from the port
             .onComplete(_ => system.terminate()) // and shutdown when done
